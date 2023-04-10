24bfae46545b600e778dc8345dbb4f1559955c1e
DongYinhang <13023695951@163.com>
2023-04-10 15:48:01 +0800

    control net img2img

--- controlNet/ControlNetImg2ImgPipline.py
+++ controlNet/ControlNetImg2ImgPipline.py
@@ -23,13 +23,26 @@ pipe_controlnet = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
 
 pipe_controlnet.scheduler = UniPCMultistepScheduler.from_config(pipe_controlnet.scheduler.config)
 pipe_controlnet.enable_xformers_memory_efficient_attention()
+pipe_controlnet.enable_model_cpu_offload()
 
 # using image with edges for our canny controlnet
 control_image = Image.open('vermeer_canny_edged.png')
 
-result_img = pipe_controlnet(controlnet_conditioning_image=control_image,
-                             image=input_image,
-                             prompt="an android robot, cyberpank, digitl art masterpiece",
-                             num_inference_steps=30).images[0]
+js = {
+    "height": 512,
+    "width": 512,
+    "num_images_per_prompt": 4,
+    "num_inference_steps": 30,
+    "prompt": "an android robot, cyberpank, digitl art masterpiece",
+    "image": [input_image] * 4,
+    "controlnet_conditioning_image": control_image,
+    "generator": [torch.Generator(device='cuda').manual_seed(12345),
+                  torch.Generator(device='cuda').manual_seed(7894389),
+                  torch.Generator(device='cuda').manual_seed(30154578),
+                  torch.Generator(device='cuda').manual_seed(8258277)]
+}
 
-result_img.save('contronet_img2img.png')
+result_img = pipe_controlnet(**js).images
+print(result_img)
+for (i, img) in enumerate(result_img):
+    img.save('contronet_img2img' + str(i) + '.png')
