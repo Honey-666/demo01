9755e03128b815c0c477437026cd97d17d28e7f8
DongYinhang <13023695951@163.com>
2023-04-11 18:01:51 +0800

    control model load

--- controlNet/Depth.py
+++ controlNet/Depth.py
@@ -7,13 +7,32 @@
 import sys
 import time
 
+import cv2
 import numpy as np
 from PIL import Image
 from transformers import pipeline
 
-depth_estimator = pipeline('depth-estimation')
 
-image = Image.open('../test_img/' + sys.argv[1])
+def resize_image(input_image, resolution):
+    H, W, C = input_image.shape
+    H = float(H)
+    W = float(W)
+    k = float(resolution) / min(H, W)
+    H *= k
+    W *= k
+    H = int(np.round(H / 64.0)) * 64
+    W = int(np.round(W / 64.0)) * 64
+    img = cv2.resize(input_image, (W, H), interpolation=cv2.INTER_LANCZOS4 if k > 1 else cv2.INTER_AREA)
+    return img
+
+
+# image = Image.open('../test_img/' + sys.argv[1])
+
+# depth_estimator = pipeline('depth-estimation', model='Intel/dpt-hybrid-midas')
+depth_estimator = pipeline('depth-estimation', model='Intel/dpt-large')
+img = cv2.imread('../img/stormtrooper.png')
+image = resize_image(img, 1360)
+image = Image.fromarray(image)
 s = time.time()
 image = depth_estimator(image)['depth']
 image = np.array(image)
@@ -22,4 +41,4 @@ image = np.concatenate([image, image, image], axis=2)
 image = Image.fromarray(image)
 spend = time.time() - s
 print(spend)
-image.save('depth_scribble_out.png')
+image.save('depth_scribble_out2-1.png')
--- controlNet/MLSD.py
+++ controlNet/MLSD.py
@@ -7,12 +7,19 @@
 import sys
 import time
 
+import torch
 from PIL import Image
 from controlnet_aux import MLSDdetector
+from controlnet_aux.mlsd import MobileV2_MLSD_Large
 
-mlsd = MLSDdetector.from_pretrained('lllyasviel/ControlNet')
+# mlsd = MLSDdetector.from_pretrained('lllyasviel/ControlNet')
+model_path = '../../models/annotator/ckpts/mlsd_large_512_fp32.pth'
+model = MobileV2_MLSD_Large()
+model.load_state_dict(torch.load(model_path), strict=True)
+mlsd = MLSDdetector(model)
 
 image = Image.open('../test_img/' + sys.argv[1])
+# image = Image.open('../img/room.png')
 s = time.time()
 image = mlsd(image)
 spend = time.time() - s
